6b3b2d6b5d55245dad56184be8e11c1995a15b03
Flzzyf <2981019122@qq.com>
2020-04-23 11:40:54 +0800

    2

--- Assets/Scripts/Editor/BuffEditor.cs
+++ Assets/Scripts/Editor/BuffEditor.cs
@@ -1,18 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class BuffEditor : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class BuffEditor : Editor {
+   
 }
